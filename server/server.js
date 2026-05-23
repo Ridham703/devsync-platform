a267c63c55a62076a882a082cee726f53a4978b6
Ridham703 <ridhammangroliya@gmail.com>
2026-05-23 13:06:23 +0530

    rate limiting checking

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.use(express.json());
 // Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 500 requests per windowMs
+  max: 5, // Limit each IP to 500 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes'
 });
 app.use('/api/', limiter);
